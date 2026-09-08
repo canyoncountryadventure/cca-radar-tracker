06f63c571dcc4ea713dca22f0cb4d5c9f8374f48
canyoncountryadventure <canyoncountryadventure@gmail.com>
2026-09-07 19:24:06 -0600

    Test Zero G logger-calibrated loss model

---
+++ cca-radar-tracker/tests/test_loss_model.py
@@ -0,0 +1,40 @@
+import unittest
+from datetime import datetime, timezone
+
+import loss_model
+
+
+class ZeroGLossModelTests(unittest.TestCase):
+    def test_august_field_calibrated_loss(self):
+        values = loss_model.zero_g_loss_components(
+            datetime(2026, 8, 15, tzinfo=timezone.utc)
+        )
+        self.assertAlmostEqual(values["eto_inches_per_day"], 7.78 / 31, places=6)
+        self.assertAlmostEqual(values["navajo_seepage_inches_per_day"], 1.28, places=6)
+        self.assertAlmostEqual(values["total_loss_inches_per_day"], 1.28 + 7.78 / 31, places=6)
+        self.assertAlmostEqual(values["percentage_points_per_day"], 1.07, delta=0.05)
+
+    def test_september_loss_is_lower_than_august(self):
+        august = loss_model.zero_g_loss_components(
+            datetime(2026, 8, 15, tzinfo=timezone.utc)
+        )
+        september = loss_model.zero_g_loss_components(
+            datetime(2026, 9, 15, tzinfo=timezone.utc)
+        )
+        self.assertLess(
+            september["percentage_points_per_day"],
+            august["percentage_points_per_day"],
+        )
+
+    def test_integrated_loss_crosses_month_boundary(self):
+        start = datetime(2026, 8, 31, 12, tzinfo=timezone.utc)
+        end = datetime(2026, 9, 1, 12, tzinfo=timezone.utc)
+        ratio = loss_model.zero_g_integrated_loss_ratio(start, end)
+        aug = loss_model.zero_g_loss_components(start)["percentage_points_per_day"]
+        sep = loss_model.zero_g_loss_components(end)["percentage_points_per_day"]
+        expected = (aug * 0.5 + sep * 0.5) / 100.0
+        self.assertAlmostEqual(ratio, expected, places=5)
+
+
+if __name__ == "__main__":
+    unittest.main()
